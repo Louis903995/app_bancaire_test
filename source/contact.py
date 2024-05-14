57370e4fa47268d79b547f48e51b77caaaa463c2
Louis Moises <louis@moises.fr>
2024-05-14 12:30:48 +0200

    wip

---
+++ source/contact.py
@@ -0,0 +1,8 @@
+class Contact:
+    def __init__(self, name, age, montant):
+        self.name = name
+        self.age = age
+        self.montant = montant
+        
+
+        
---
+++ source/operation.py
@@ -0,0 +1,7 @@
+def add(number_one, number_two):
+    return number_one + number_two
+
+def to_subtract(number_one, number_two):
+    if number_one > number_two:
+        return number_one - number_two
+
