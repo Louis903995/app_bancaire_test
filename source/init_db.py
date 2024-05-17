5bfa58c923803528c360235bd0688e0c292df928
Louis Moises <louis@moises.fr>
2024-05-17 15:55:17 +0200

    sauvegarde

--- source/init_db.py
+++ source/init_db.py
@@ -1,5 +1,3 @@
-# Initialiser connexion à la BDD
-
 from sqlalchemy import Column, Integer, String, Float, ForeignKey, create_engine
 from sqlalchemy.orm import relationship, declarative_base
 import sqlite3
@@ -7,16 +5,29 @@ import sqlite3
 Base = declarative_base()
 
 # Établir une connexion à la base de données 
-connexion = sqlite3.connect('BDD.db')
+connexion = sqlite3.connect('bank.db')
 
 # curseur
 curseur = connexion.cursor()
 
-# Créer une table
+# Création Account_Table
+
 curseur.execute('''CREATE TABLE IF NOT EXISTS utilisateurs (
                     id INTEGER PRIMARY KEY,
                     account_id INTEGER,
-                    balance INTEGER, 
+                    balance INTEGER
+                  )''')
+
+curseur.execute("INSERT INTO utilisateurs (account_id, balance) VALUES ('234', '3000')")
+curseur.execute("INSERT INTO utilisateurs (account_id, balance) VALUES ('889', '9678')")
+
+connexion.commit()
+connexion.close()
+
+# Création Transaction_Table
+
+curseur.execute('''CREATE TABLE IF NOT EXISTS utilisateurs (
+                    id INTEGER PRIMARY KEY,
                     transaction_id INTEGER,
                     count_id INTEGER,
                     amount INTEGER,
@@ -24,10 +35,8 @@ curseur.execute('''CREATE TABLE IF NOT EXISTS utilisateurs (
                     timestamp INTEGER
                   )''')
 
-curseur.execute("INSERT INTO utilisateurs (account_id, balance, transaction_id, count_id, amount, type, timestamp) VALUES ('234', '3000', '5', '6', '7', '8', '8')")
-curseur.execute("INSERT INTO utilisateurs (account_id, balance, transaction_id, count_id, amount, type, timestamp) VALUES ('889', '9678', '5', '6', '7', '8', '9')")
-curseur.execute("INSERT INTO utilisateurs (account_id, balance, transaction_id, count_id, amount, type, timestamp) VALUES ('889', '9678', '5', '6', '7', '8', '5')")
-curseur.execute("INSERT INTO utilisateurs (account_id, balance, transaction_id, count_id, amount, type, timestamp) VALUES ('889', '9678', '5', '6', '7', '8', '4')")
+curseur.execute("INSERT INTO utilisateurs (transaction_id, count_id, amount, type, timestamp) VALUES ('5', '6', '7', '8', '8')")
+curseur.execute("INSERT INTO utilisateurs (transaction_id, count_id, amount, type, timestamp) VALUES ('5', '6', '7', '8', '9')")
 
 connexion.commit()
 connexion.close()
