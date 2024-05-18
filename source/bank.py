29ca8b08f47b2df9f2dd542a706458e7c5ca37e8
Louis Moises <louis@moises.fr>
2024-05-19 00:20:39 +0200

    wip

--- source/bank.py
+++ source/bank.py
@@ -1,75 +1,44 @@
-from sqlalchemy import Column, Integer, String, Float, ForeignKey, create_engine
-from sqlalchemy.orm import relationship, declarative_base
-import sqlite3
-
-Base = declarative_base()
-
-# Établir une connexion à la base de données 
-connexion = sqlite3.connect('bank.db')
-
-# curseur
-curseur = connexion.cursor()
-
-class Transaction:
-    # Création Transaction_Table
-        curseur.execute('''CREATE TABLE IF NOT EXISTS transactions (
-                    id INTEGER PRIMARY KEY,
-                    transaction_id INTEGER,
-                    count_id INTEGER,
-                    amount INTEGER,
-                    type INTEGER,
-                    timestamp INTEGER
-                  )''')
-
-        # curseur.execute("INSERT INTO transactions (transaction_id, count_id, amount, type, timestamp) VALUES ('5', '6', '7', '8', '8')")
-
-        connexion.commit()
-
-    
+class Account:
     def __init__(self, id, solde_initial=0):
         self.id = id
-        self.solde = solde_initial
-
-    def deposit(self, montant):
-        self.solde += montant
-
-    def withdraw(self, montant):
-        if montant > self.solde:
-            print("Fonds insuffisants.")
-        else:
-            self.solde -= montant
-
-    def get_balance(self):
-        print(f"Solde du compte de {self.id} : {self.solde}")
-
-class Account:
-    def __init__():
-        # Création Account_Table
-        curseur.execute('''CREATE TABLE IF NOT EXISTS utilisateurs (
-                    id INTEGER PRIMARY KEY,
-                    account_id INTEGER,
-                    balance INTEGER
-                  )''')
-
-        # curseur.execute("INSERT INTO utilisateurs (account_id, balance) VALUES ('234', '3000')")
-
-        connexion.commit()
-        connexion.close()
+        self.solde_initial = solde_initial
+    
+    def afficher_infos(self):
+        print(f"Numéro du compte: {self.id}, Solde: {self.solde_initial}")
+    
+    def solde_du_compte(self, montant):
+        self.solde_initial += montant
 
 
-        def __init__(self, nom_base_donnees):
-            self.connexion = sqlite3.connect(nom_base_donnees)
-            self.curseur = self.connexion.cursor()
-            self.curseur.execute('''CREATE TABLE IF NOT EXISTS comptes (nom TEXT PRIMARY KEY, solde NUMERIC)''')
-            self.connexion.commit()
+class Transaction:
+    def __init__(self, transaction_id, account_id, amount, type, timestamp, solde_compte):
+        self.transaction_id = transaction_id
+        self.account_id = account_id
+        self.amount = amount
+        self.type = type
+        self.timestamp = timestamp
+        self.solde_compte = solde_compte
 
-    def creer_compte(self, nom, solde_initial=0):
-        try:
-            self.curseur.execute('''INSERT INTO comptes VALUES (?, ?)''', (nom, solde_initial))
-            self.connexion.commit()
-            print(f"Compte de {nom} créé avec succès.")
-        except sqlite3.IntegrityError:
-            print(f"Le compte de {nom} existe déjà.")
+    def afficher_infos(self):
+        print(f"Le compte {self.account_id} a fait un {self.type} de {self.amount}$, transaction: {self.transaction_id}, solde compte: {self.solde_compte}$, heure: {self.timestamp}")
 
-    def fermer(self):
-        self.connexion.close()
+    def deposit(self, somme):
+        self.solde_compte += somme
+    
+    def withdraw(self, somme):
+        self.solde_compte -= somme
+
+
+transaction = Transaction(281654, 454564, 100, 'retait', '8h25', 785)
+transaction.afficher_infos()
+print('-----')
+transaction.deposit(15)
+transaction.afficher_infos()
+print('-----')
+transaction.withdraw(100)
+transaction.afficher_infos()
+
+
+
+
+
--- source/init_db.py
+++ source/init_db.py
@@ -3,11 +3,7 @@ from sqlalchemy.orm import relationship, declarative_base
 import sqlite3
 
 Base = declarative_base()
-
-# Établir une connexion à la base de données 
 connexion = sqlite3.connect('bank.db')
-
-# curseur
 curseur = connexion.cursor()
 
 # Création Account_Table
@@ -34,5 +30,39 @@ curseur.execute('''CREATE TABLE IF NOT EXISTS transactions (
 # curseur.execute("INSERT INTO transactions (transaction_id, count_id, amount, type, timestamp) VALUES ('5', '6', '7', '8', '8')")
 
 connexion.commit()
-
 connexion.close()
+
+
+class Transaction:
+    def __init__(self, id, solde_initial=0):
+        self.id = id
+        self.solde = solde_initial
+
+    def deposit(self, montant):
+        self.solde += montant
+
+    def withdraw(self, montant):
+        if montant > self.solde:
+            print("Fonds insuffisants.")
+        else:
+            self.solde -= montant
+
+    def get_balance(self):
+        print(f"Solde du compte de {self.id} : {self.solde}")
+
+class Account:
+    def __init__(self, id, account_id, balance):
+        self.id = id
+        self.account_id = account_id
+        self.balance = balance
+        
+    def creer_compte(self, id, solde_initial=0):
+        try:
+            self.curseur.execute('''INSERT INTO Account VALUES (, )''', (id, solde_initial))
+            self.connexion.commit()
+            print(f"Compte numéro {id} créé avec succès.")
+        except sqlite3.IntegrityError:
+            print(f"Le compte {id} existe déjà.")
+
+    def fermer(self):
+        self.connexion.close()
