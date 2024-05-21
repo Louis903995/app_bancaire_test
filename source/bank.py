889d1a0551993e3dd99dbf71cba2c3c5a74f1d4e
Louis Moises <louis@moises.fr>
2024-05-21 16:38:23 +0200

    wip

--- source/bank.py
+++ source/bank.py
@@ -47,16 +47,3 @@ class Transaction:
             print(f'Solde insuffisant pour effectuer le retrait de {somme}$')
 
 
-# Exemple d'utilisation
-compte = Account(12345, 1000)
-transaction = Transaction(compte, '', '')
-
-
-"""
-# Tentative de retrait de 1500$ (devrait échouer)
-transaction.withdraw(500)
-compte.afficher_infos()
-"""
-# Dépôt de 500$
-transaction.deposit(4000)
-compte.afficher_infos()
--- source/example_app.py
+++ source/example_app.py
@@ -1,3 +1,16 @@
 # exemple de transaction
 
+compte = Account(12345, 1000)
+transaction = Transaction(compte, '', '')
+
+
+"""
+# Tentative de retrait de 1500$ (devrait échouer)
+transaction.withdraw(500)
+compte.afficher_infos()
+"""
+# Dépôt de 500$
+transaction.deposit(4000)
+compte.afficher_infos()
+
 
