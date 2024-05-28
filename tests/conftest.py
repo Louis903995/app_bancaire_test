27800b23303a24fc39881146a7c7d9b7ed6844b5
Louis Moises <louis@moises.fr>
2024-05-28 16:30:48 +0200

    wip

--- tests/conftest.py
+++ tests/conftest.py
@@ -1,1 +1,17 @@
 # Contient différentes fixtures 
+import pytest
+#from source.bank import Account, Transaction
+import sys
+sys.path.append('/document/app_bancaire_test/source/bank')
+
+
+
+# fixture pour créer une instance de la classe Account
+@pytest.fixture 
+def account_instance():
+    return source.bank.Account(1, 8)
+
+
+
+
+
